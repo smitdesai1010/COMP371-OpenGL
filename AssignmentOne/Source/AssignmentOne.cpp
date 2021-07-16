50a5c3d57b624fb6c2ff2f671a0b80dc2bbdc2c7
Piyush Goyal <60353847+Piyush-prog@users.noreply.github.com>
2021-07-16 17:04:57 -0400

    Cube placed at center of world matrix.

--- AssignmentOne/Source/AssignmentOne.cpp
+++ AssignmentOne/Source/AssignmentOne.cpp
@@ -171,17 +171,85 @@ int createVertexArrayObject()
   return vertexArrayObject;
 }
 
-int createVertexArrayObject(float x, float y, float z, float r, float g, float b)
+int createCubeVertexArrayObject(/*float x, float y, float z, float r, float g, float b*/)
 {
-    // A vertex is a point on a polygon, it contains positions and other data (eg: colors)
-    glm::vec3 vertexArray[] = {
-        glm::vec3(x,  y, -z),  // top right
-        glm::vec3(r,  g, b),  // top right color (red)
-        glm::vec3(-x, y, z),  // bottom right
-        glm::vec3(r,  g, b),  // top right color (red)
-        glm::vec3(x, -y, z),  // bottom left
-        glm::vec3(r,  g, b),  // top right color (red)
 
+    GLfloat vertexArray[] = {
+    -0.5f,-0.5f,-0.5f, // triangle 1 : begin
+    -0.5f,-0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f, // triangle 1 : end
+    0.5f, 0.5f,-0.5f, // triangle 2 : begin
+    -0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f, // triangle 2 : end
+    0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+    0.5f,-0.5f,-0.5f,
+    0.5f, 0.5f,-0.5f,
+    0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+    0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+    0.5f,-0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f,-0.5f,-0.5f,
+    0.5f, 0.5f,-0.5f,
+    0.5f,-0.5f,-0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f,-0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f,
+    0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f,
+    0.5f,-0.5f, 0.5f
+    };
+
+    GLfloat g_color_buffer_data[] = {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
     };
 
     // Create a vertex array
@@ -196,21 +264,27 @@ int createVertexArrayObject(float x, float y, float z, float r, float g, float b
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, GL_STATIC_DRAW);
 
+
+
     glVertexAttribPointer(0,                   // attribute 0 matches aPos in Vertex Shader
         3,                   // size
         GL_FLOAT,            // type
         GL_FALSE,            // normalized?
-        2 * sizeof(glm::vec3), // stride - each vertex contain 2 vec3 (position, color)
+        0, // stride - each vertex contain 2 vec3 (position, color)
         (void*)0             // array buffer offset
     );
     glEnableVertexAttribArray(0);
 
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     glVertexAttribPointer(1,                            // attribute 1 matches aColor in Vertex Shader
         3,
         GL_FLOAT,
         GL_FALSE,
-        2 * sizeof(glm::vec3),
+        0,
         (void*)sizeof(glm::vec3)      // color is offseted a vec3 (comes after position)
     );
     glEnableVertexAttribArray(1);
@@ -220,6 +294,8 @@ int createVertexArrayObject(float x, float y, float z, float r, float g, float b
 
     return vertexArrayObject;
 }
+
+
 
 
 int main(int argc, char*argv[])
@@ -270,9 +346,7 @@ int main(int argc, char*argv[])
     
     // Define and upload geometry to the GPU here ...
     int vao = createVertexArrayObject();
-    int vaoFrontBack = createVertexArrayObject(0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f);
-    int vaoTopBottom = createVertexArrayObject(0.5f, 0.0f, 0.5f, 0.0f, 1.0f, 0.0f);
-    int vaoLeftRight = createVertexArrayObject(0.0f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f);
+    int vaoCube = createCubeVertexArrayObject();
 
     
     // Enable Backface culling
@@ -351,123 +425,17 @@ int main(int argc, char*argv[])
 
         glDrawArrays(GL_LINES, 0, 2);
 
-
         glBindVertexArray(0);
-
-
-        // Draw Front-Back geometry
+        
+        //Draw Cube
         glUseProgram(shaderProgram);
-        glBindVertexArray(vaoFrontBack);
-        glEnable(GL_CULL_FACE);
+        glBindVertexArray(vaoCube);
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+        worldMatrix = scalingMatrix;
 
-        //front face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.5f));
-        worldMatrix = translationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.5f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //back face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -0.5f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -0.5f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(1.0f, -1.0f, 0.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
         glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glBindVertexArray(0);
-
-
-        // Draw Top-Bottom geometry
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vaoTopBottom);
-
-        //Top Face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.5f, 0.0f));
-        worldMatrix = translationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.5f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //Bottom face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 1.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(1.0f, 0.0f, -1.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-        glBindVertexArray(0);
-
-
-        // Draw Left-Right geometry
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vaoLeftRight);
-
-        //Left Face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f));
-        worldMatrix = translationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //Right face
-        // triangle 1
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, -1.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //triangle 2
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, 0.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 1.0f));
-        worldMatrix = translationMatrix * rotationMatrix;
-        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-        glBindVertexArray(0);
-
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // End Frame
@@ -621,21 +589,3 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
         fov = 1.0f;
 }
 
-
-
-
-
-
-
-/*
-            eyePosition = glm::vec3(deltaX, 0.0f,0.0f);
-
-            glm::mat4 viewMatrix = glm::lookAt(
-                eyePosition,  // eye
-                eyePosition+glm::vec3(0.0f, 0.0f, -1.0f),    //center
-                glm::vec3(0.0f, 1.0f, 0.0f));   // up
-
-
-            GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
-            glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
-            */
